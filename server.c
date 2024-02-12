7a9e6c909989404bd0a4720ac7c1229a522ebb7f
Suren <surenenfiajyan@gmail.com>
2024-02-13 01:56:13 +0400

    Save changes

--- server.c
+++ server.c
@@ -1,6 +1,18 @@
 #include "util.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc < 2)
+	{
+		puts("Please provide port number");
+	}
+
+	int portNumber = atoi(argv[1]);
+
+	if (portNumber < 0 || portNumber > 65535 )
+	{
+		puts("Please provide port number within range 0 - 65535");
+	}
+
 	return 0;
 }
