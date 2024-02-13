1d2f9ba40632fbc4b4af8a36d29649555ed52320
Suren <suren.enfiajyan@naghashyan.com>
2024-02-13 12:57:10 +0400

    Changes

--- server.c
+++ server.c
@@ -5,13 +5,15 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		puts("Please provide port number");
+		return 0;
 	}
 
 	int portNumber = atoi(argv[1]);
 
 	if (portNumber < 0 || portNumber > 65535 )
 	{
-		puts("Please provide port number within range 0 - 65535");
+		puts("Please provide a port number within range 0 - 65535");
+		return 0;
 	}
 
 	return 0;
